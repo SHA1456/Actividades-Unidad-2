024aee716908b5bb38b34919f6a4f1cb9ba28515
SHA1456 <heredia.acero.sergio@iescamas.es>
2025-09-23 20:58:42 +0200

    Add files via upload

---
+++ Actividad5.java
@@ -0,0 +1,28 @@
+package com;
+import java.util.Locale; //Para poder usar puntos
+import java.util.Scanner;
+public class Actividad5 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		sc.useLocale(Locale.US);
+		
+		
+		double numero;
+		
+		System.out.println("Introduzca un número decimal: ");
+		numero=sc.nextDouble();
+		
+		if(numero>-1 && numero<1 && numero!=0) {
+			
+			System.out.println("Es un número casi cero");
+			
+			
+			
+		}else {
+			System.out.println("No es un número casi cero");
+		}
+		sc.close();
+	}
+
+}
