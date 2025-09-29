8e322d422517b49c59f615a2377a0ff8207d1e0d
SHA1456 <heredia.acero.sergio@iescamas.es>
2025-09-29 12:51:00 +0200

    Add files via upload

---
+++ Act7.java
@@ -0,0 +1,52 @@
+package com;
+import java.util.Scanner;
+public class Act7 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num1,num2,num3;
+		
+		System.out.println("Introduzca el primer número: ");
+		num1=sc.nextInt();
+		
+		System.out.println("Introduzca el segundo número: ");
+		num2=sc.nextInt();
+		
+		System.out.println("Introduzca el tercer número: ");
+		num3=sc.nextInt();
+		
+		if (num1>num2&&num2>num3) {
+			System.out.println(num1 + "," + num2+"," + num3);
+			
+		}else if(num1>num3&&num3>num2){
+			
+			System.out.println(num1+","+num3+","+num2);
+			
+		}else if(num2>num1&&num1>num3) {
+			System.out.println(num2+","+num1+","+num3);
+			
+		}else if(num2>num3&&num3>num1) {
+			System.out.println(num2+","+num3+","+num1);
+			
+			
+		}else if(num3>num1&&num1>num2) {
+			
+			System.out.println(num3+","+num1+","+num2);
+			
+			
+		}else if(num3>num2&&num2>num1) {
+			
+			System.out.println(num3+","+num2+","+num1);
+			
+		}
+		
+		
+		
+		
+		sc.close();
+		
+
+	}
+
+}
